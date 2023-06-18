c7c9740867099aa366dd4d06ba6626abc982f611
chessplanet86 <chessplanet86@yandex.ru>
2023-06-18 23:02:51 +0500

    Промежуточные изменения. Сделан пункт с блокировкой пользователей.

--- client.py
+++ client.py
@@ -1,10 +1,55 @@
 # import asyncio
 # import aiohttp
+import logging
+import socket
+import threading
 
 
 class Client:
-    def __init__(self, server_host="127.0.0.1", server_port=8000):
-        pass
+    def __init__(self, server_host: str = "127.0.0.1", server_port: int = 8000, nickname: str = ''):
+        self.server_host = server_host
+        self.server_port = server_port
+        self.login = nickname
+        self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    def send(self, message=""):
-        pass
+    def receive(self) -> None:
+        while True:
+            try:
+                """Метод выводит все сообщения, которые пришли на сервер от других пользователей"""
+                message = self.client.recv(1024).decode('utf-8')
+                if message == 'NICK':
+                    self.client.send(self.login.encode('utf-8'))
+                else:
+                    print(message)
+            except Exception as e:
+                """В сдучае ошибки отрубаем соединение"""
+                logging.error(f"Произошла ошибка {e}")
+                self.client.close()
+                break
+
+    def write(self) -> None:
+        while True:
+            message = f'{input()}'
+            self.client.send(message.encode('utf-8'))
+
+    def connect_to_server(self) -> None:
+        # Подключаемся к серверу
+        self.client.connect((self.server_host, self.server_port))
+
+        # Запускаем поток на получение данных от Сервера
+        receive_thread = threading.Thread(target=self.receive)
+        receive_thread.start()
+
+        # Запускаем поток на посылку данных на сервер
+        write_thread = threading.Thread(target=self.write)
+        write_thread.start()
+
+
+if __name__ == '__main__':
+    logging.basicConfig(
+        level=logging.INFO,
+        format='[%(asctime)s] %(levelname).1s %(message)s',
+        datefmt='%Y.%m.%d %H:%M:%S',
+    )
+    client = Client(nickname=input('Введите логин свой логин\n'))
+    client.connect_to_server()
--- server.py
+++ server.py
@@ -1,9 +1,170 @@
 # import asyncio
+import socket
+import threading
+import logging
+from datetime import datetime, timedelta
+from collections import deque
+import os
+import shutil
+import pickle
+from collections import defaultdict
 
 
 class Server:
-    def __init__(self, host="127.0.0.1", port=8000):
-        pass
+    def __init__(self, host: str = "127.0.0.1", port: int = 8000, max_length_history: int = 20, ban_time: int = 4):
+        """Инициализируем входные параметры сервера"""
+        self.host = host
+        self.port = port
+        self.clients = []
+        self.nicknames = []
+        self.dict_clients = {}
+        self.type_socket = socket.AF_INET  # Определяем тип соккета. Используем интернет сокет
+        self.protocol = socket.SOCK_STREAM  # Определяем протокол. Используем TCP (transaction control protocol)
+        self.banusers = defaultdict(lambda: dict(time=datetime.now(), foul=0))
+        self.ban_time = ban_time
+        try:
+            with open(f"history_chat.txt", 'rb') as f:
+                data = pickle.load(f)
+            if isinstance(data, deque):
+                self.history = data
+            else:
+                self.history = deque()
+        except FileNotFoundError:
+            self.history = deque()
+        self.max_length_history = max_length_history
+
+    def broadcast(self, message: bytes, nickname: str):
+        """Вспомогательная функция для трансляции сообщений всем клиентам, подключенным к серверу"""
+        for client in self.clients:
+            client.send(datetime.now().strftime("%Y-%m-%d %H:%M:%S ").encode('utf-8') + f'({nickname}-общий): '.encode(
+                'utf-8') + message)
+
+    def private_cast(self, client, msg: bytes, from_user: str):
+        client.send(
+            datetime.now().strftime("%Y-%m-%d %H:%M:%S ").encode('utf-8') + f'({from_user}-лс): '.encode('utf-8') + msg)
+
+    @staticmethod
+    def get_to_user_name(message):
+        """Метод определяет юзера которому будет адресовано сообщение или бан"""
+        k = 0
+        for sb in message:
+
+            if sb == ' ':
+                k += 1
+                break
+            else:
+                k += 1
+
+        to_user = message[1:k].strip()
+        return to_user, message[k:].strip()
+
+    def handle(self, client, nickname):
+        while True:
+            try:
+                message = client.recv(1024)  # Получаем сообщение от клиента
+                if self.banusers[nickname]['foul'] >= 3:
+                    if datetime.now() - self.banusers[nickname]['time'] < timedelta(hours=self.ban_time):
+                        time_endban = self.banusers[nickname]['time'] + timedelta(hours=self.ban_time)
+                        self.private_cast(client=client,
+                                          msg=f'Вы забанены. Бан истечет в  {time_endban}'.encode('utf-8'),
+                                          from_user='server')
+                        continue
+                    else:
+                        self.banusers[nickname]['foul'] = 0
+                decode_message = message.decode('utf-8')
+
+                message_history = datetime.now().strftime("%Y-%m-%d %H:%M:%S ").encode(
+                    'utf-8') + f'{nickname}: '.encode('utf-8') + message
+                if len(self.history) < self.max_length_history:
+                    self.history.append(message_history)
+                else:
+                    self.history.append(message_history)
+                    self.history.popleft()
+
+                if not os.path.exists('test/'):
+                    os.mkdir('test')
+
+                shutil.rmtree("history_chat.txt", ignore_errors=True)
+                with open("history_chat.txt", 'wb') as f:
+                    pickle.dump(self.history, f, protocol=pickle.HIGHEST_PROTOCOL)
+
+                if decode_message[0] == '#':
+                    to_user, _ = self.get_to_user_name(decode_message)
+                    self.banusers[to_user]['foul'] += 1
+                    if self.banusers[to_user]['foul'] == 3:
+                        self.banusers[to_user]['time'] = datetime.now()
+                        self.private_cast(client=self.dict_clients.get(to_user),
+                                          msg='Вы забанены на 4 часа'.encode('utf-8'), from_user='server')
+                    else:
+                        if self.banusers[to_user]['foul'] < 3:
+                            self.broadcast(message=f'{to_user} получил предупреждение'.encode('utf-8'),
+                                           nickname='server')
+                        else:
+                            self.private_cast(client=client,
+                                              msg=f'{to_user} уже забанен'.encode('utf-8'), from_user='server')
+                    continue
+
+                if decode_message[0] == '@':
+                    to_user, msg = self.get_to_user_name(decode_message)
+                    self.private_cast(client=self.dict_clients.get(to_user),
+                                      msg=msg.encode('utf-8'), from_user=nickname)
+                else:
+                    logging.info('Трансляция сообщения всем клиентам')
+                    self.broadcast(message=message, nickname=nickname)
+
+            except Exception as e:
+                """
+                В случае возникновения ошибки удаляем клиента и закрываем соединение
+                """
+                logging.error(f'Пользователь {nickname} удален по причине {e}')
+                index = self.clients.index(client)
+                self.clients.remove(client)
+                client.close()
+                nickname = self.nicknames[index]
+                self.broadcast(f'{nickname} покинул чат!'.encode('utf-8'),
+                               nickname='server')  # Кодируем в байты наше сообщение
+                self.nicknames.remove(nickname)
+                break
+
+    def receive(self):
+        server = socket.socket(self.type_socket, self.protocol)
+        server.bind((self.host, self.port))
+        server.listen()
+        logging.info(f"Сервер хост: {self.host} ожидает подключения на порту: {self.port}")
+        while True:
+            # Accept Connection
+            client, address = server.accept()
+            logging.info(f"Клиент с адресом {str(address)} подключен к серверу. Пожалуйста, представься.")
+
+            # Request And Store Nickname
+            client.send('NICK'.encode('utf-8'))
+            nickname = client.recv(1024).decode('utf-8')
+            self.nicknames.append(nickname)
+            self.clients.append(client)
+            self.dict_clients[nickname] = client
+            # Print And Broadcast Nickname
+            print(f"Клиент с адресом {str(address)} представился как {nickname}")
+            self.broadcast(f"{nickname} подключился!".encode('utf-8'), nickname='server')
+            client.send('Вы успешно подключены!\n'.encode('utf-8'))
+            for mes in self.history:
+                client.send(mes + b'\n')
+
+            # Запускаем поток для клиента
+            thread = threading.Thread(target=self.handle, args=(client, nickname))
+            thread.start()
 
     def listen(self):
-        pass
+        """Запуск сервера, который слушает подключения клиентов"""
+
+        self.receive()
+
+
+if __name__ == '__main__':
+    logging.basicConfig(
+        level=logging.INFO,
+        format='[%(asctime)s] %(levelname).1s %(message)s',
+        datefmt='%Y.%m.%d %H:%M:%S',
+    )
+
+    server = Server()
+    server.listen()
